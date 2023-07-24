405d3e3d8c5302173228f25673874a2db5faa575
Patrick Long <patrick@deepspacerobots.com>
2023-07-24 07:02:49 -0500

    initial commit

---
+++ http_tcpserver.cpp
@@ -0,0 +1,168 @@
+#include "./http_tcpserver.h"
+
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+
+namespace
+{
+  const int BUFFER_SIZE = 30720;
+
+  void log(const std::string& message)
+  {
+    std::cout << message << std::endl;
+  }
+
+  void exitWithError(const std::string& errorMessage)
+  {
+    std::cout << WSAGetLastError() << std::endl;
+    log("ERROR: " + errorMessage);
+    exit(1);
+  }
+}
+
+namespace http
+{
+  TcpServer::TcpServer(std::string ip_address, int port)
+    : m_ip_address(ip_address),
+      m_port(port),
+      m_socket(),
+      m_new_socket(),
+      m_incomingMessage(),
+      m_socketAddress(),
+      m_socketAddress_len(sizeof(m_socketAddress)),
+      m_serverMessage(buildResponse())
+  {
+    m_socketAddress.sin_family = AF_INET;
+    m_socketAddress.sin_port = htons(m_port);
+    m_socketAddress.sin_addr.s_addr = inet_addr(m_ip_address.c_str());
+
+    if (startServer() != 0)
+    {
+      std::ostringstream ss;
+      ss << "Failed to start server with PORT: " << ntohs(m_socketAddress.sin_port);
+      log(ss.str());
+    }
+  } // TcpServer::TcpServer
+
+  TcpServer::~TcpServer()
+  {
+    closeServer();
+  } // TcpServer::~TcpServer
+
+  int TcpServer::startServer()
+  {
+    if (WSAStartup(MAKEWORD(2, 0), &m_wsaData) != 0)
+    {
+      exitWithError("WSAStartup failed");
+    }
+
+    m_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (m_socket == INVALID_SOCKET)
+    {
+      exitWithError("Cannot create socket");
+      return 1;
+    }
+
+    if (bind(m_socket, (sockaddr*)&m_socketAddress, m_socketAddress_len) < 0)
+    {
+      exitWithError("Cannot connect socket to address");
+      return 1;
+    }
+
+    return 0;
+  } // TcpServer::startServer
+
+  void TcpServer::closeServer()
+  {
+    closesocket(m_socket);
+    closesocket(m_new_socket);
+    WSACleanup();
+    exit(0);
+  } // TcpServer::closeServer
+
+  void TcpServer::startListen()
+  {
+    if (listen(m_socket, 20) < 0)
+    {
+      exitWithError("Socket listen failed");
+    }
+
+    std::ostringstream ss;
+    ss << "\n*** Listening on ADDRESS: " << inet_ntoa(m_socketAddress.sin_addr) << " PORT: " << ntohs(m_socketAddress.sin_port) << " ***\n\n";
+    log(ss.str());
+
+    int bytesReceived;
+    while (true)
+    {
+      log("===== Waiting for a new connection =====\n\n\n");
+      acceptConnection(m_new_socket);
+
+      char buffer[BUFFER_SIZE] = { 0 };
+      bytesReceived = recv(m_new_socket, buffer, BUFFER_SIZE, 0);
+
+      if (bytesReceived < 0)
+      {
+        exitWithError("Failed to receive bytes from client socket connection");
+      }
+
+      std::ostringstream ss;
+      ss << "----- Received Request from client -----\n\n\n";
+      log(ss.str());
+
+      sendResponse();
+
+      closesocket(m_new_socket);
+    }
+  } // TcpServier::startListen
+
+  void TcpServer::acceptConnection(SOCKET& new_socket)
+  {
+    new_socket = accept(m_socket, (sockaddr*)&m_socketAddress, &m_socketAddress_len);
+
+    if (new_socket == INVALID_SOCKET)
+    {
+      std::ostringstream ss;
+      ss << "Server failed to accept incoming connection from ADDRESS: " << inet_ntoa(m_socketAddress.sin_addr) << "; PORT: " << ntohs(m_socketAddress.sin_port);
+      exitWithError(ss.str());
+    }
+  } // TcpServer::acceptConnection
+
+  std::string TcpServer::buildResponse()
+  {
+    std::string htmlFile = "<!DOCTYPE html><html lang=\"en\"><body><h1> HOME </h1><p> Hello from your Server :) </p></body></html>";
+
+    std::ostringstream ss;
+    ss << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " << htmlFile.size() << "\n\n" << htmlFile;
+
+    return ss.str();
+  } // TcpServer::buildResponse
+
+  void TcpServer::sendResponse()
+  {
+    int bytesSent;
+    long totalBytesSent = 0;
+
+    while (totalBytesSent < m_serverMessage.size()) 
+    {
+      bytesSent = send(m_new_socket, m_serverMessage.c_str(), m_serverMessage.size(), 0);
+
+      if (bytesSent < 0)
+      {
+        break;
+      }
+
+      totalBytesSent += bytesSent;
+    }
+
+    if (totalBytesSent == m_serverMessage.size())
+    {
+      log("----- Server Response sent to client -----\n\n");
+    }
+    else
+    {
+      log("Error sending response to client.");
+    }
+  } // TcpServer::sendResponse
+} // namespace http
---
+++ server.cpp
@@ -0,0 +1,8 @@
+#include "./http_tcpserver.h"
+
+int main()
+{
+  using namespace http;
+  TcpServer server = TcpServer("127.0.0.1", 8080);
+  return 0;
+}
