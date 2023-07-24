7297121436149bf0051f06e2fad1a25a2970340e
Patrick Long <patrick@deepspacerobots.com>
2023-07-24 09:32:24 -0500

    adjust .sh script to compile and run C++ server

--- server.sh
+++ server.sh
@@ -1,2 +1,4 @@
-g++ -std=c++11 -Wall server.cpp http_tcpserver.cpp -o server -lws2_32
+g++ -c server.cpp -o server.o
+g++ -c http_tcpserver.cpp -o http_tcpserver.o
+g++ -std=c++11 -Wall server.o http_tcpserver.o -o server -lws2_32
 ./server
