715a96888c73f4158f12b450ac620c7a2597b545
Patrick Long <patrick@deepspacerobots.com>
2023-07-31 09:40:40 -0500

    upgrade g++ compiler to use c++ v 20

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@ echo "Compiling files..."
 
 g++ -c server.cpp -o server.o
 g++ -c http_tcpserver.cpp -o http_tcpserver.o
-g++ -std=c++11 -Wall server.o http_tcpserver.o -o server -lws2_32
+g++ -std=c++20 -Wall server.o http_tcpserver.o -o server -lws2_32
 
 echo "Finished compiling"
 echo "Starting server..."
