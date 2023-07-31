1d389541a27a27dd95bac756376ba6cafaccefd2
Patrick Long <patrick@deepspacerobots.com>
2023-07-31 07:42:46 -0500

    add some helpful process logging to server script

--- server.sh
+++ server.sh
@@ -1,4 +1,10 @@
+echo "Compiling files..."
+
 g++ -c server.cpp -o server.o
 g++ -c http_tcpserver.cpp -o http_tcpserver.o
 g++ -std=c++11 -Wall server.o http_tcpserver.o -o server -lws2_32
+
+echo "Finished compiling"
+echo "Starting server..."
+
 ./server
